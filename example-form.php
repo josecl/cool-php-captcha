c4e85084ac5ad755310bfbe1e20d8f17e014736c
joserodriguezvalderrama <joserodriguezvalderrama@afd741cb-174f-0410-8c1a-552ecd46ae06>
2008-09-26 14:21:22 +0000

    BUG: session_start() mal puesto en script example-form.php

--- example-form.php
+++ example-form.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -18,7 +19,7 @@ body { font-family: sans-serif; font-size: 0.8em; padding: 20px; }
 <?php
 
 
-session_start();
+
 
 
 
@@ -54,6 +55,7 @@ if (!empty($_REQUEST['captcha'])) {
         </table>
         </div>
 HTML;
+    unset($_SESSION['captcha']);
 }
 
 
