a27bf988168b238f54a059fc4bb7e280d9436472
joserodriguezvalderrama <joserodriguezvalderrama@afd741cb-174f-0410-8c1a-552ecd46ae06>
2009-02-23 03:51:47 +0000

    Simple autodetect language example (english or spanish)

--- captcha.php
+++ captcha.php
@@ -16,16 +16,45 @@ session_start();
 
 $captcha = new SimpleCaptcha();
 
-// Change configuration...
+
+
+// OPTIONAL Change configuration...
 //$captcha->wordsFile = 'words/es.php';
 //$captcha->session_var = 'secretword';
 //$captcha->imageFormat = 'png';
 //$captcha->scale = 3; $captcha->blur = true;
 
+// OPTIONAL Simple autodetect language example
+/*
+if (!empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+    $langs = array('en', 'es');
+    $lang  = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    if (in_array($lang, $langs)) {
+        $captcha->wordsFile = "words/$lang.php";
+    }
+}
+*/
 
 
 
+// Image generation
 $captcha->CreateImage();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
